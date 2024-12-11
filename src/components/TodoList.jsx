41490878f1364c2dd9d4d6d9087625641611a3f6
Dhanush CK <dhanushck01@gmail.com>
2024-12-12 00:14:01 +0530

    feat : Added a portion for completed task

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -8,24 +8,24 @@ function TodoList() {
     const handleEdit = (id) => {        
         setTodos((prev) => 
             prev.map((todo) => 
-                todo.id === id ? {...todo, count: todo.count++, isEditable : !todo.isEditable} :todo
+                todo.id === id ? {...todo, isEditable : !todo.isEditable} :todo
             )
         )
-    }
+    };
 
-    const handleChange = (text ,id) => {        
+    const handleChange = (text ,id) => {   
         setTodos((prev) => 
             prev.map((todo) => 
                 todo.id === id ? {...todo, todoText : text} :todo
             )
         )
-    }
+    };
 
     const handleEnter = (e, id) => {
         if(e.key === "Enter") {
             handleEdit(id)
         }
-    }
+    };
 
     const handleTask = (id) => {
         setTodos((prev) => 
@@ -36,21 +36,42 @@ function TodoList() {
     }
 
     return (
+        <div className="holder">
         <div className="lists">
             {
-                todos.map((todo)=> {
+                todos.filter((todo)=>!todo.isDone).map((todo)=> {
                     return <div key={todo.id} className="task">
-                        <input type="text" className="text" value={todo.todoText} readOnly={!todo.isEditable} onChange={(e)=>{handleChange(e.value, todo.id)}} onKeyDown={(e)=>handleEnter( e,todo.id)}></input>
                         <div className="keys">
+                            <input type="text" className="text" value={todo.todoText} readOnly={!todo.isEditable} onChange={(e)=>{handleChange(e.target.value, todo.id)}} onKeyDown={(e)=>handleEnter( e,todo.id)}></input>
                             <input type="checkbox" className="checkbox" checked= {todo.isDone} onChange={()=>handleTask(todo.id)}/>
-                            <div className="buttons">
-                                <button className="delete" onClick={()=>deleteTodo(todo.id)}>Delete</button>
-                                <button className="edit" onClick={()=>handleEdit(todo.id)}>{todo.isEditable ? "Done": "Edit"}</button>
-                            </div>
+                        </div>
+                        <div className="buttons">
+                            <button className="delete" onClick={()=>deleteTodo(todo.id)}>Delete</button>
+                            <button className="edit" onClick={()=>handleEdit(todo.id)}>{todo.isEditable ? "Done": "Edit"}</button>
                         </div>
                     </div>
                 })
             }
+        </div>
+        <div className="completed">
+            <h2 className="completed-text">Completed</h2>
+        </div>
+        <div className="done">
+            {
+                todos.filter((todo)=>todo.isDone).map((todo) => {
+                    return <div key={todo.id} className="task">
+                        <div className="keys">
+                            <input type="text" className="text" value={todo.todoText} readOnly={!todo.isEditable} onChange={(e)=>{handleChange(e.target.value, todo.id)}} onKeyDown={(e)=>handleEnter( e,todo.id)}></input>
+                            <input type="checkbox" className="checkbox" checked= {todo.isDone} onChange={()=>handleTask(todo.id)}/>
+                        </div>
+                        <div className="buttons">
+                            <button className="delete" onClick={()=>deleteTodo(todo.id)}>Delete</button>
+                            <button className="edit" onClick={()=>handleEdit(todo.id)}>{todo.isEditable ? "Done": "Edit"}</button>
+                        </div>
+                    </div>
+                })
+            }
+        </div>
         </div>
     )
 }
